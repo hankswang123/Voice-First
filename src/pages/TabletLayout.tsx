b534ac3a595e02c6009e8e9950f3ceb75e52706f
Wang <hanks.wang@sap.com>
2025-10-04 06:36:17 +0800

    Tablet for both pad&&mobile

--- src/pages/TabletLayout.tsx
+++ src/pages/TabletLayout.tsx
@@ -3786,7 +3786,7 @@ export function TabletLayout() {
                             renderAnnotationLayer={true}
                             onLoadSuccess={() => onPageLoadSuccess({ pageNumber })}
                             loading={<p>Loading page {pageNumber}...</p>}
-                            width={isTwoPageView ? 800 : 920}
+                            width={isTwoPageView ? 600 : 900}
                           />
                         </div>
                       ))}
